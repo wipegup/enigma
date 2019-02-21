18d2584ac726e1bb9527261069b412f945e4b0dc
wipegup <wipegup@gmail.com>
2019-02-21 15:10:17 -0700

    Test it can pad a number to correct length to string

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -2,5 +2,12 @@ class ShiftTest <MiniTest::Test
   def setup
     @enigma = Enigma.new
   end
+
+  def test_it_pads_random_number
+    raw = 78
+    expected = "00078"
+
+    assert_equal expected, @enigma.pad(raw)
+  end
   
 end
