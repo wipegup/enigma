3d1afc83c2abedf16f7ded4664de82eb5f0b0718
wipegup <wipegup@gmail.com>
2019-02-24 06:37:10 -0700

    Back after demonstration

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -47,7 +47,6 @@ class ShiftTest < MiniTest::Test
   end
 
   def test_it_can_shift_alphabet
-
     expected = ["c","d","e","f","g","h","i","j","k","l","m","n","o",
                 "p","q","r","s","t","u","v","w","x","y","z"," ","a","b"]
     assert_equal expected, @enigma.shift(2)
