1c6049d82e38618f85d6d3e9e63e474de83df2df
wipegup <wipegup@gmail.com>
2019-02-25 08:25:16 -0700

    Add new methods to encrypt call

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,8 +5,9 @@ class Enigma
   include Shift
 
   def encrypt(message, key = nil, date = nil)
-    date = int_from_date(today).to_s if date == nil
-    key = pad(generate_random_number) if key == nil
+    date = todays_date if date == nil
+    key = random_key if key == nil
     message = message.downcase
+    
   end
 end
