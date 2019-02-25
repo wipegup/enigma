4cb153e9e0161b466f29f5a73800e0cc2c6ce10c
wipegup <wipegup@gmail.com>
2019-02-25 07:26:50 -0700

    Add test for creation from shift

--- test/alphabet_test.rb
+++ test/alphabet_test.rb
@@ -3,12 +3,9 @@ require './lib/alphabet'
 
 class AlphabetTest < MiniTest::Test
 
-  def setup
-    @alphabet = Alphabet.new
-  end
-
   def test_it_exists
-    assert_instance_of Alphabet, @alphabet
+    alphabet = Alphabet.new
+    assert_instance_of Alphabet, alphabet
   end
 
   def test_it_can_create_alphabet
@@ -23,4 +20,11 @@ class AlphabetTest < MiniTest::Test
                 "p","q","r","s","t","u","v","w","x","y","z"," ","a","b"]
     assert_equal expected, @alphabet.shift_alphabet(2)
   end
+
+  def test_it_can_create_alphabet_with_shift
+    expected = ["c","d","e","f","g","h","i","j","k","l","m","n","o",
+                "p","q","r","s","t","u","v","w","x","y","z"," ","a","b"]
+    alphabet = Alphabet.with_shift(2)
+    assert_equal expected, alphabet.alphabet
+
 end
