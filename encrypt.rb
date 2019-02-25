6b92d8f3dec7075676c5366aa2c832b15465bc49
wipegup <wipegup@gmail.com>
2019-02-25 16:51:47 -0700

    Finish functions for use of command line

--- encrypt.rb
+++ encrypt.rb
@@ -2,5 +2,8 @@ require './lib/enigma'
 
 input, output = ARGV
 
-
+message = IO.read(input).gsub("\n","")
+enigma = Enigma.new
+encrypted = enigma.encrypt(message)
+IO.write(output, encrypted[:encryption])
 puts "Created '#{output}' with the key #{enigma.key} and date #{enigma.date}"
