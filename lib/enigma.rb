7385fbfb029ba83d5a4868820ec5ab96ff0bba25
wipegup <wipegup@gmail.com>
2019-02-25 08:18:36 -0700

    Outline of encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,12 @@
 require_relative './shift.rb'
+require_relative './alphabet.rb'
+
 class Enigma
   include Shift
-  def initialize
 
+  def encrypt(message, key = nil, date = nil)
+    date = int_from_date(today).to_s if date == nil
+    key = pad(generate_random_number) if key == nil
+    message = message.downcase
   end
 end
