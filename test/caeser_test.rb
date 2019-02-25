03fc26967e76caf11d14e303c95dcd1f8a3db39e
wipegup <wipegup@gmail.com>
2019-02-25 10:08:44 -0700

    Reconfigure tests for Caeser Class

--- test/caeser_test.rb
+++ test/caeser_test.rb
@@ -1,71 +1,27 @@
 require './test/test_helper'
-require './lib/alphabet'
+require './lib/caeser'
 
-class AlphabetTest < MiniTest::Test
+class CaeserTest < MiniTest::Test
 
   def test_it_exists
-    alphabet = Alphabet.new
-    assert_instance_of Alphabet, alphabet
-  end
-
-  def test_it_can_create_alphabet
-
-    expected = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
-                "n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
-    assert_equal expected, Alphabet.alphabet
-  end
-
-  def test_it_can_shift_alphabet
-    alphabet = Alphabet.new
-    expected = ["c","d","e","f","g","h","i","j","k","l","m","n","o",
-                "p","q","r","s","t","u","v","w","x","y","z"," ","a","b"]
-    assert_equal expected, alphabet.shift_alphabet(2)
-  end
-
-  def test_it_can_create_alphabet_with_shift
-    expected = ["c","d","e","f","g","h","i","j","k","l","m","n","o",
-                "p","q","r","s","t","u","v","w","x","y","z"," ","a","b"]
-    alphabet = Alphabet.with_shift(2)
-    assert_equal expected, alphabet.alphabet
+    assert_instance_of Caeser, Caeser.new
   end
 
   def test_it_returns_correct_encoded_letter
-    alphabet = Alphabet.with_shift(2)
-    assert_equal "c", alphabet.encode("a")
-    assert_equal "d", alphabet.encode("b")
+    assert_equal "c", Caeser.encode("a", 2)
+    assert_equal "d", Caeser.encode("b", 2)
 
-    alphabet = Alphabet.new
-    assert_equal "a", alphabet.encode("a")
-    assert_equal "b", alphabet.encode("b")
-  end
-
-  def test_it_finds_index_of_letter
-    alphabet = Alphabet.new
-
-    assert_equal 0, alphabet.index_of("a")
-    assert_equal 1, alphabet.index_of("b")
-    assert_equal 25, alphabet.index_of("z")
-    assert_equal 26, alphabet.index_of(" ")
-  end
-
-  def test_it_finds_letter_from_index
-    alphabet = Alphabet.new
-    assert_equal "a", alphabet.char_from_index(0)
-    assert_equal "b", alphabet.char_from_index(1)
-    assert_equal "z", alphabet.char_from_index(25)
-    assert_equal " ", alphabet.char_from_index(26)
+    assert_equal "a", Caeser.encode("a", 0)
+    assert_equal "b", Caeser.encode("b", 0)
   end
 
   def test_it_can_decode_characters
-    alphabet = Alphabet.with_shift(2)
-    assert_equal "a", alphabet.decode("c")
-    assert_equal "b", alphabet.decode("d")
-    assert_equal "y", alphabet.decode(" ")
+    assert_equal "a", Caeser.decode("c", 2)
+    assert_equal "b", Caeser.decode("d", 2)
+    assert_equal "y", Caeser.decode(" ", 2)
 
-    alphabet = Alphabet.new
-    assert_equal "v", alphabet.decode("v")
-    assert_equal " ", alphabet.decode(" ")
+    assert_equal "v", Caeser.decode("v", 0)
+    assert_equal " ", Caeser.decode(" ", 0)
   end
-
-
+  
 end
