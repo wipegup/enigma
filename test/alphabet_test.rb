8e2aa8cd837c69bdbad9488aba4e940f03774b61
wipegup <wipegup@gmail.com>
2019-02-25 07:20:38 -0700

    Edit tests for different class

--- test/alphabet_test.rb
+++ test/alphabet_test.rb
@@ -2,16 +2,25 @@ require './test/test_helper'
 require './lib/alphabet'
 
 class AlphabetTest < MiniTest::Test
+
+  def setup
+    @alphabet = Alphabet.new
+  end
+
+  def test_it_exists
+    assert_instance_of Alphabet, @alphabet
+  end
+
   def test_it_can_create_alphabet
 
     expected = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
                 "n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
-    assert_equal expected, @enigma.alphabet
+    assert_equal expected, Alphabet.alphabet
   end
 
   def test_it_can_shift_alphabet
     expected = ["c","d","e","f","g","h","i","j","k","l","m","n","o",
                 "p","q","r","s","t","u","v","w","x","y","z"," ","a","b"]
-    assert_equal expected, @enigma.shift_alphabet(2)
+    assert_equal expected, @alphabet.shift_alphabet(2)
   end
 end
