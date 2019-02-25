1be4211e7b753b82357a4727000a938682a26d21
wipegup <wipegup@gmail.com>
2019-02-25 10:11:01 -0700

    Saving tests for indexing helpers

--- test/caeser_test.rb
+++ test/caeser_test.rb
@@ -18,10 +18,30 @@ class CaeserTest < MiniTest::Test
   def test_it_can_decode_characters
     assert_equal "a", Caeser.decode("c", 2)
     assert_equal "b", Caeser.decode("d", 2)
-    assert_equal "y", Caeser.decode(" ", 2)
+    assert_equal "y", Caeser.decode(" ",2)
 
     assert_equal "v", Caeser.decode("v", 0)
     assert_equal " ", Caeser.decode(" ", 0)
   end
-  
+
+  def test_it_finds_index_of_letter
+    alphabet = Alphabet.new
+
+    assert_equal 0, alphabet.index_of("a")
+    assert_equal 1, alphabet.index_of("b")
+    assert_equal 25, alphabet.index_of("z")
+    assert_equal 26, alphabet.index_of(" ")
+  end
+
+  def test_it_finds_letter_from_index
+    alphabet = Alphabet.new
+    assert_equal "a", alphabet.char_from_index(0)
+    assert_equal "b", alphabet.char_from_index(1)
+    assert_equal "z", alphabet.char_from_index(25)
+    assert_equal " ", alphabet.char_from_index(26)
+  end
+
+
+
+
 end
