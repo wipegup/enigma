049a29329450b3ba12d596e36fa5d0e0148b6cf4
wipegup <wipegup@gmail.com>
2019-02-25 12:13:11 -0700

    Passing ciphertext tests

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,8 +8,16 @@ class Enigma
     date = todays_date if date == nil
     key = random_key if key == nil
 
-    shifts = generate_shifts(key, date)
+    shifts = generate_shifts( date,key)
     text = create_ciphertext(message.downcase, shifts)
     return {key:key, date:date, encryption: text }
+  end
+
+  def create_ciphertext(message, shifts)
+    valid_chars = message.chars.find_all{ |char| Caeser.alphabet.include?(char)}
+    encrypted_chars = valid_chars.map.with_index do |char, index|
+       Caeser.encode(char, shifts[index%4])
+    end
+    return encrypted_chars.join("")
   end
 end
