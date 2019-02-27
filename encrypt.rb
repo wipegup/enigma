dc79df70ced183678ff9f1ea6fd2a6a658e8d4cc
wipegup <wipegup@gmail.com>
2019-02-26 22:55:47 -0700

    Change key and date access to the dictionary

--- encrypt.rb
+++ encrypt.rb
@@ -6,4 +6,4 @@ message = IO.read(input).gsub("\n","")
 enigma = Enigma.new
 encrypted = enigma.encrypt(message)
 IO.write(output, encrypted[:encryption])
-puts "Created '#{output}' with the key #{enigma.key} and date #{enigma.date}"
+puts "Created '#{output}' with the key #{encrypted[:key]} and date #{encrypted[:date]}"
